268e9df1ce32264f80323e28c4cbb1d464301ad4
“Emma <ekc30@duke.edu>
2023-11-19 14:24:54 -0500

    recommender is working but everything is very arbitrary lol

--- recommendersystem.py
+++ recommendersystem.py
@@ -1,42 +1,79 @@
 import math
+from collections import defaultdict
+
 
 interactions = []
+
+tweet_text = {}
+
 
 # Read ratings.csv. For each item in ratings, ratings have form: userid, movieid, rating, timestamp.
 
 with open("data/fake_interactions.csv", "r") as file:
+    k = 0
     for line in file:
-        li = line.strip().split(",")
-        interactions.append(li)
+        if (k == 0):
+            pass
+        else:
+            li = line.strip().split(",")
+            interactions.append(li)
+        k = k + 1
+
+with open("data/monkeypox.csv", "r") as file:
+    k = 0
+    for line in file:
+        if (k == 0):
+            pass
+        else:
+            li = line.strip().split(",")
+            tweet_text[int(li[0])] = li[2]
+        k = k + 1
 
 #Create dictionary of users and the tweets they have interacted with.
 #Users 
 
+# interactions_dict = {}
+# for interaction in interactions:
+#     user = interaction[0]
+#     tweet = interaction[1]
+#     interaction_type = interaction[2]
+#     if (user not in interactions_dict.keys()):
+#         interactions_dict[user] = {}
+#     else:
+#         if (tweet not in in interactions_dict[user].keys()):
+#             interactions_dict[user][tweet] = [0] * 3           
+#     if (interaction_type == "like"):
+#         interactions_dict[user][tweet][0] = 1
+#     if (interaction_type == "comment"):
+#         interactions_dict[user][tweet][1] = 1
+#     if (interaction_type == "retweet"):
+#         interactions_dict[user][tweet][2] = 1
 interactions_dict = {}
+userset = set()
 for interaction in interactions:
-    user = interaction[0]
-    tweet = interaction[1]
+    user = int(interaction[0])
+    tweet = int(interaction[1])
     interaction_type = interaction[2]
+    userset.add(user)
     if (user not in interactions_dict.keys()):
         interactions_dict[user] = {}
-    else:
-        if (tweet not in in interactions_dict[user].keys()):
-            interactions_dict[user][tweet] = [0] * 3           
+    if (tweet not in interactions_dict[user].keys()):
+        interactions_dict[user][tweet] = 0           
     if (interaction_type == "like"):
-        interactions_dict[user][tweet][0] = 1
+        interactions_dict[user][tweet] = interactions_dict[user][tweet] + 1
     if (interaction_type == "comment"):
-        interactions_dict[user][tweet][1] = 1
+        interactions_dict[user][tweet] = interactions_dict[user][tweet] + 2
     if (interaction_type == "retweet"):
-        interactions_dict[user][tweet][2] = 1
+        interactions_dict[user][tweet] = interactions_dict[user][tweet] + 3
 
 
-userset = set()
-for rating in ratings:
-    if (int(rating[0]) in users):
-        users[int(rating[0])].append([int(rating[1]), int(rating[2])])
-    else:
-        users[int(rating[0])] = [[int(rating[1]), int(rating[2])]]
-    userset.add(int(rating[0]))
+# userset = set()
+# for rating in ratings:
+#     if (int(rating[0]) in users):
+#         users[int(rating[0])].append([int(rating[1]), int(rating[2])])
+#     else:
+#         users[int(rating[0])] = [[int(rating[1]), int(rating[2])]]
+#     userset.add(int(rating[0]))
 
 
 def angulardistance(combinedlist):
@@ -54,15 +91,12 @@ def angulardistance(combinedlist):
 def ratingdistance(user1, user2, threshold):
     combined = defaultdict(list)
     combinedlist = []
-
-    for movie1 in users[user1]:
-        combined[movie1[0]].append(movie1[1])
-
-    for movie2 in users[user2]:
-        combined[movie2[0]].append(movie2[1])
-        if (len(combined[movie2[0]]) > 1):
-            combinedlist.append(combined[movie2[0]])
-
+    for tweet in interactions_dict[user1].keys():
+        combined[tweet].append(interactions_dict[user1][tweet])
+    for tweet in interactions_dict[user2].keys():
+        combined[tweet].append(interactions_dict[user2][tweet])
+        if (len(combined[tweet]) > 1):
+            combinedlist.append(combined[tweet])
     if (len(combinedlist) > threshold - 1):
         return angulardistance(combinedlist)
     else:
@@ -83,25 +117,25 @@ def knearestneighbor(u, S, threshold, k):
     return(ret)
 
 def recommender(u, nrecs, k):
-    users2 = users.copy()
+    interactions_copy = interactions_dict.copy()
     neighbors = knearestneighbor(u, userset, 3, k)
     movies = defaultdict(list)
-    for movierating in users2[u]:
-        movies[movierating[0]] = ["PASS"]
+    for tweet in interactions_copy[u].keys():
+        movies[tweet] = ["PASS"]
     for neighbor in neighbors:
-        for movierating in users2[neighbor]:
-            if (movierating[0] in movies):
-                if (movies[movierating[0]] != ["PASS"]):
-                    movies[movierating[0]][0] += 1
-                    movies[movierating[0]][1] = (movies[movierating[0]][1] + movierating[1])
+        for tweet in interactions_copy[neighbor].keys():
+            if (tweet in movies.keys()):
+                if (movies[tweet] != ["PASS"]):
+                    movies[tweet][0] += 1
+                    movies[tweet][1] = (movies[tweet][1] + interactions_copy[neighbor][tweet])
             else:
-                movies[movierating[0]] = [1, movierating[1]]
+                movies[tweet] = [1, interactions_copy[neighbor][tweet]]
     smoothedprediction = []
-    for movie, data in movies.items():
+    for tweet, data in movies.items():
         if (data != ["PASS"]):
             average = data[1]/data[0]
-            prediction = (3.5 + (data[0]*average))/(1 + data[0])
-            smoothedprediction.append([movie, prediction])
+            prediction = (1 + (data[0]*average))/(1 + data[0])
+            smoothedprediction.append([tweet_text[tweet], prediction])
     smoothedprediction.sort(key=lambda x: x[1], reverse=True)
     return smoothedprediction[0:nrecs]
 
@@ -111,7 +145,9 @@ def main():
     # print(ratingdistance(200, 500, 3))
     # print(knearestneighbor(2, (100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110), 3, 5))
     # print(recommender(1, 5, 30))
-    print(recommender(0, 5, 30))
+    recommendations = recommender(1, 10, 30)
+    for line in recommendations:
+        print(line)
 
 if __name__ == "__main__":
     main()
